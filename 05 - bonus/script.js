762176b9d658b7ef93182b310b42dbc53ffb8ff7
Abraxas-7 <arber.beshaj.02@gmail.com>
2024-11-06 15:43:49 +0100

    bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -9,10 +9,22 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function greet(inputName) {
+    let currentHour = new Date().getHours();
+    if(currentHour < 13) {
+        return `Buongiorno ${inputName}!`;
+    } 
+    else if (currentHour < 17) {
+        return `Buonpomeriggio ${inputName}!`;
+    }
+    else {
+        return `Buonasera ${inputName}!`;
+    }
+}   
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const result = greet(name);
+console.log(result);
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
